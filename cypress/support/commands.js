1bdee8ef57ce94dc5b93a829a1221f1abc06b1a6
Pogryziony <Pogryziony98@interia.pl>
2024-05-01 14:52:03 +0200

    Add generated files for demo purpose

---
+++ cypress/support/commands.js
@@ -0,0 +1,5 @@
+/*
+This file is used to define custom commands that you can use in your tests.
+For purpose of this task, this file is empty.
+However, here can be defined custom commands that can be reusable across multiple tests.
+ */
---
+++ cypress/support/e2e.js
@@ -0,0 +1,12 @@
+/*
+    This file is generated automatically by Cypress.
+    It is typically used to configure end-to-end (E2E) testing settings.
+    It allows you to define options such as the base URL for the tests, test file paths, browser preferences,
+    and any environment variables specific to E2E testing.
+ */
+
+// Import commands.js using ES2015 syntax:
+import './commands'
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
